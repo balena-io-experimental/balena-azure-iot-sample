2c64a118a5319182db38c2fbd047e41a19588b49
tameraw <tameraw@microsoft.com>
2015-09-23 10:27:26 -0700

    Initial portal solution

---
+++ tools/DeviceExplorerPortal/DeviceExplorerPortal/Models/DeviceEntity.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DeviceExplorerPortal.Models
+{
+    public class DeviceEntity
+    {
+        public string Id { get; set; }
+        public string PrimaryKey { get; set; }
+        public string SecondaryKey { get; set; }
+        public string ConnectionString { get; set; }
+        public string ConnectionState { get; set; }
+        public DateTime LastActivityTime { get; set; }
+        public DateTime LastConnectionStateUpdatedTime { get; set; }
+        public DateTime LastStateUpdatedTime { get; set; }
+        public int MessageCount { get; set; }
+        public string State { get; set; }
+        public string SuspensionReason { get; set; }
+
+        public override string ToString()
+        {
+            return String.Format("Device ID = {0}, Primary Key = {1}, Secondary Key = {2}, ConnectionString = {3}, ConnState = {4}, ActivityTime = {5}, LastConnState = {6}, LastStateUpdatedTime = {7}, MessageCount = {8}, State = {9}, SuspensionReason = {10}\r\n",
+                        this.Id,
+                        this.PrimaryKey,
+                        this.SecondaryKey,
+                        this.ConnectionString,
+                        this.ConnectionState,
+                        this.LastActivityTime,
+                        this.LastConnectionStateUpdatedTime,
+                        this.LastStateUpdatedTime,
+                        this.MessageCount,
+                        this.State,
+                        this.SuspensionReason);
+        }
+    }
+}
---
+++ tools/DeviceExplorerPortal/DeviceExplorerPortal/Models/DevicesProcessor.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Azure.Devices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeviceExplorerPortal.Models
+{
+    public class DevicesProcessor
+    {
+        private List<DeviceEntity> listOfDevices;
+        private RegistryManager registryManager;
+        private String iotHubConnectionString;
+        private int maxCountOfDevices;
+        private String protocolGatewayHostName;
+
+        public DevicesProcessor(string iotHubConnenctionString, int devicesCount, string protocolGatewayName)
+        {
+            this.listOfDevices = new List<DeviceEntity>();
+            this.iotHubConnectionString = iotHubConnenctionString;
+            this.maxCountOfDevices = devicesCount;
+            this.protocolGatewayHostName = protocolGatewayName;
+            this.registryManager = RegistryManager.CreateFromConnectionString(iotHubConnectionString);
+        }
+
+        public async Task<List<DeviceEntity>> GetDevices()
+        {
+            try
+            {
+                DeviceEntity deviceEntity;
+                var devices = await registryManager.GetDevicesAsync(maxCountOfDevices);
+
+                if (devices != null)
+                {
+                    foreach (var device in devices)
+                    {
+                        deviceEntity = new DeviceEntity()
+                        {
+                            Id = device.Id,
+                            ConnectionState = device.ConnectionState.ToString(),
+                            ConnectionString = CreateDeviceConnectionString(device),
+                            LastActivityTime = device.LastActivityTime,
+                            LastConnectionStateUpdatedTime = device.ConnectionStateUpdatedTime,
+                            LastStateUpdatedTime = device.StatusUpdatedTime,
+                            MessageCount = device.CloudToDeviceMessageCount,
+                            State = device.Status.ToString(),
+                            SuspensionReason = device.StatusReason
+                        };
+
+                        if (device.Authentication != null &&
+                            device.Authentication.SymmetricKey != null)
+                        {
+                            deviceEntity.PrimaryKey = device.Authentication.SymmetricKey.PrimaryKey;
+                            deviceEntity.SecondaryKey = device.Authentication.SymmetricKey.SecondaryKey;
+                        }
+
+                        listOfDevices.Add(deviceEntity);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return listOfDevices;
+        }
+
+        private String CreateDeviceConnectionString(Device device)
+        {
+            StringBuilder deviceConnectionString = new StringBuilder();
+
+            var hostName = String.Empty;
+            var tokenArray = iotHubConnectionString.Split(';');
+            for (int i = 0; i < tokenArray.Length; i++)
+            {
+                var keyValueArray = tokenArray[i].Split('=');
+                if (keyValueArray[0] == "HostName")
+                {
+                    hostName = tokenArray[i] + ';';
+                    break;
+                }
+            }
+
+            if (!String.IsNullOrWhiteSpace(hostName))
+            {
+                deviceConnectionString.Append(hostName);
+                deviceConnectionString.Append("CredentialScope=Device;");
+                deviceConnectionString.AppendFormat("DeviceId={0}", device.Id);
+
+                if (device.Authentication != null &&
+                    device.Authentication.SymmetricKey != null)
+                {
+                    deviceConnectionString.AppendFormat(";SharedAccessKey={0}", device.Authentication.SymmetricKey.PrimaryKey);
+                }
+
+                if (this.protocolGatewayHostName.Length > 0)
+                {
+                    deviceConnectionString.AppendFormat(";GatewayHostName=ssl://{0}:8883", this.protocolGatewayHostName);
+                }
+            }
+
+            return deviceConnectionString.ToString();
+        }
+    }
+}

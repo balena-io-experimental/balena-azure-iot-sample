b74fd4d4406480e4911e1a88969ba4e75df743f5
tameraw <tameraw@microsoft.com>
2015-09-23 10:27:26 -0700

    Initial portal solution

---
+++ tools/DeviceExplorerPortal/DeviceExplorerPortal/Controllers/ManagementController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DeviceExplorerPortal.Controllers
+{
+    public class ManagementController : Controller
+    {
+        // GET: Management
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Management/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Management/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Management/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Management/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Management/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Management/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Management/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
